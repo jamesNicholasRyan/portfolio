6ad71d28b1ad6aab5134bac7b173884736c97cc0
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-03-25 20:55:06 +0000

    falling leaves aded

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -10,21 +10,25 @@ class Tree extends React.Component {
   Tree = (p) => {
     // Bubble[] bubbles = new Bubble[10]
     const canvasHeight = 2000
-    const bubbleCount = 30
+    const leaveCount = 76
     const dustCount = 50
-    let bubbles = []
+    let leaves = []
     let dusts = []
     var canvas
     const dustSpeed = [-0.3, 0.3]
-    const bubbleSpeed = [-10, 10]
+    // const bubbleSpeed = [-10, 10]
+    let angle = p.PI/6
+    const leafSpeed = 0.7
+    const gravity = p.createVector(0, 0.001)
 
     p.setup = () => {
       canvas = p.createCanvas(p.windowWidth, canvasHeight)
       canvas.position(0,0)
       canvas.style('z-index', '-1')
-      for (let i = 0; i < bubbleCount; i++) {
-        const bubble = new Bubble(p.random(p.windowWidth), p.random(canvasHeight), p.random(1,45), randomSpeed(bubbleSpeed), randomSpeed(bubbleSpeed))
-        bubbles.push(bubble)
+      for (let i = 0; i < leaveCount; i++) {
+        const leaf = new Leaf(p.random(1)*p.width, p.random(175, 250), 1, 0, 0, p.random(1,385))
+        leaf.calculateAge()
+        leaves.push(leaf)
       }
       for (let i = 0; i < dustCount; i++) {
         const dust = new Dust(p.random(p.windowWidth), p.random(canvasHeight), p.random(4,7), randomSpeed(dustSpeed), randomSpeed(dustSpeed))
@@ -37,37 +41,34 @@ class Tree extends React.Component {
       const color1 = p.color(18,126,190)
       const color2 = p.color(255)
       setGradient(0,0,p.width,p.height,color1,color2)
-      bubbles.forEach((bubble) => {
-        bubble.display()
-        bubble.move()
-        bubble.drag()
-        bubble.checkSides()
-        bubble.checkDeath()
-        // bubble.boyancy()
-
-        const dist = bubble.location.dist(mouseVector)
-        if ((dist < 100)) {
-          bubble.applyForce(pushingForce(bubble).mult(-1))
-        }
-      })
       dusts.forEach((dust) => {
         dust.display()
         dust.move()
         dust.checkSides()
       })
-      
-      bubbles.forEach((bubble) => {
-        if (bubble.lifeSpan < 0) {
-          if ((bubbles.length < 60) && (!bubble.outOfBounds)) {
-            for (let i = 0; i < 2; i++) {
-              const newBubble = new Bubble(bubble.location.x, bubble.location.y, p.random(10,30), randomSpeed(bubbleSpeed), randomSpeed(bubbleSpeed))
-              bubbles.push(newBubble)
-            }
-          }
-          const index = bubbles.indexOf(bubble)
-          bubbles.splice(index, 1)
+      leaves.forEach((leaf) => {
+        leaf.display()
+        leaf.move()
+        leaf.grow()
+
+        if (leaf.isFalling) {
+          leaf.applyForce(gravity)
         }
+        if (leaf.isDead) {
+          const index = leaves.indexOf(leaf)
+          leaves.splice(index, 1)
+        }
+        // leaf.drag()
+
+        // const dist = leaf.location.dist(mouseVector)
+        // if ((dist < 100)) {
+        //   leaf.applyForce(pushingForce(leaf).mult(-1))
+        // }
       })
+      
+      // leaves.forEach((leaf) => {
+      //   
+      // })
 
       dusts.forEach((dust) => {
         if (dust.isDead === true) {
@@ -78,38 +79,32 @@ class Tree extends React.Component {
         }
       })
 
-      if (bubbles.length < 50) {
-        const bubble = new Bubble(p.random(p.windowWidth), p.random(canvasHeight), p.random(1,5), randomSpeed(bubbleSpeed), randomSpeed(bubbleSpeed))
-        bubbles.push(bubble)
+      if (leaves.length < 80) {
+        console.log('makin more leaves!')
+        const leaf = new Leaf(p.random(1)*p.width, p.random(175, 250), 1, 0, 0, 1)
+        leaf.calculateAge()
+        leaves.push(leaf)
       }
-      drawTree()
-      p.stroke(255,0,0)
-      p.translate(p.width/2, p.height)
-      fractal(100)
-    }
 
-    function volcanoes(x, y) {
-      const bubbleSpeedUp = [15, 20]
-      const bubbleSpeedSide = [-3, 3]
-      const volcanoBubble = new Bubble(x, y, p.random(1,5), randomSpeed(bubbleSpeedSide), randomSpeed(bubbleSpeedUp))
-      bubbles.push(volcanoBubble)
-      const interval = randomSpeed([1200,2000])
-      setTimeout(() => volcanoes(x, y), interval)
+
+      drawTree()
+      drawTopBox()
+
     }
 
     p.windowResized = () => {
-      p.resizeCanvas(p.windowWidth, p.windowHeight)
+      p.resizeCanvas(p.windowWidth, canvasHeight)
     }
 
-    p.mousePressed = () => {
-      bubbles.forEach((bubble) => {
-        bubble.clicked()
-      })
-    }
+    // p.mousePressed = () => {
+    //   bubbles.forEach((bubble) => {
+    //     bubble.clicked()
+    //   })
+    // }
 
-    function pushingForce(bubble) {
+    function pushingForce(leaf) {
       const mousePosition = p.createVector(p.mouseX, p.mouseY)
-      const force = mousePosition.sub(bubble.location)
+      const force = mousePosition.sub(leaf.location)
       let distance = force.mag()
       distance = p.constrain(distance,5.0,25.0)
 
@@ -142,10 +137,6 @@ class Tree extends React.Component {
       p.vertex(p.width/5, p.height-80)
       p.vertex(p.width/4, p.height-70)
       p.vertex(p.width/3, p.height-120)
-      p.vertex(p.width*0.4, p.height/2)
-      p.vertex(p.width*0.4, 0)
-      p.vertex(p.width*0.6, 0)
-      p.vertex(p.width*0.6, p.height/2)
       p.vertex(p.width*0.666, p.height-120)
       p.vertex(p.width*0.75, p.height-70)
       p.vertex(p.width*0.80, p.height-80)
@@ -156,14 +147,43 @@ class Tree extends React.Component {
       p.endShape(p.CLOSE)
     }
 
-    function fractal(len) {
-      const angle = p.random(0,p.PI/3)
+    function drawTopBox() {
+      p.fill(0, 14, 30)
+      p.beginShape()
+      p.vertex(0, 0)
+      p.vertex(p.width, 0)
+      p.vertex(p.width, 175)
+      p.vertex(0, 175)
+      p.endShape(p.CLOSE)
+    }
 
+    function fractalTrees() {
+      p.stroke(0, 15, 30)
+      p.push()
+      p.translate(p.width*0.07, 0)
+      fractal(100)
+      for (let i = 0; i < 6; i++) {
+        p.translate(p.width*0.14, -100)
+        fractal(100)
+      }
+      p.pop()
+      p.translate(p.width*0.07, 0)
+      fractal(100)
+      for (let i = 0; i < 6; i++) {
+        p.translate(p.width*0.7, -200)
+        fractal(100)
+      }
+    }
+
+    function fractal(len) {
+      // angle = p.random(0,p.PI/3)
+      // const angle = p.PI/3
+      
       p.strokeWeight(8)
-      p.line(0, 0, 0, -len)
-      p.translate(0, -len)
+      p.line(0, 0, 0, len)
+      p.translate(0, len)
       len *= 0.67
-      if (len > 2) {
+      if (len > 5) {
         p.push()
         p.rotate(angle)
         fractal(len)
@@ -174,6 +194,7 @@ class Tree extends React.Component {
         p.pop()
       }
     }
+
 
 
     
@@ -202,7 +223,7 @@ class Tree extends React.Component {
       move() {
         this.velocity.add(this.acceleration)
         this.location.add(this.velocity)
-        // this.acceleration.mult(0)
+        this.acceleration.mult(0)
       }
 
       applyForce(force) {
@@ -229,85 +250,128 @@ class Tree extends React.Component {
     }
 
 // -------------------------------------------------------------------------------------------------------------------- //
-    class Bubble extends Particle {
-      constructor(X, Y, d, v1, v2) {
-        super(X, Y, d, v1, v2)
+    class Leaf extends Particle {
+      constructor(X, Y, d, v1, v2, lifeSpan) {
+        super(X, Y, d, v1, v2, lifeSpan)
+        this.location = p.createVector(X, Y)
+        this.acceleration = p.createVector(0, 0)
+        this.velocity = p.createVector(v1, v2)
+        this.diameter = d
+        this.isDead = false
+        this.lifeSpan = lifeSpan
+        this.outOfBounds = false
+        this.color1 = p.color(0, 255, 140)
+        this.colorArray = [0, 255, 140]
+        this.isFalling = false
       }
 
-      checkDeath() {
-        if (this.lifeSpan > 50) {
+      display() {
+        p.noStroke()
+        p.fill(this.color1)
+        p.rect(this.location.x, this.location.y, this.diameter)
+      }
+
+      calculateAge() {
+        if (this.lifeSpan < 130) {
+          const gap = 140-this.lifeSpan
+          this.color1 = p.color(0, 255, gap)
+          this.colorArray = [0, 255, gap]
+        } else if (this.lifeSpan < 385) {
+          const gap = this.lifeSpan-130
+          this.color1 = p.color(gap, 255, 10)
+          this.colorArray = [gap, 255, 10]
+        } else if (this.lifeSpan < 640) {
+          const gap = this.lifeSpan-385
+          const gap2 = 255-gap
+          this.color1 = p.color(255, gap2, 10)
+          this.colorArray = [255, gap2, 10]
+        }
+        if (this.lifeSpan < 20) {
+          this.diameter = this.lifeSpan
+        } else {
+          this.diameter = 20
+        }
+      }
+
+      grow() {
+        if (this.diameter < 20) {
+          this.diameter = this.diameter + leafSpeed/9
+        }
+
+        if (this.lifeSpan < 130) {
+          // console.log('blue now!')
+          this.colorArray[2] = this.colorArray[2] - leafSpeed/2
+          this.lifeSpan = this.lifeSpan + leafSpeed/2
+        } else if (this.lifeSpan < 385) {
+          // console.log('red now!')
+          this.colorArray[0] = this.colorArray[0] + leafSpeed/4
+          this.lifeSpan = this.lifeSpan + leafSpeed/4
+        } else if (this.lifeSpan < 640) {
+          // console.log('green now!')
+          this.colorArray[1] = this.colorArray[1] - leafSpeed/5
+          this.lifeSpan = this.lifeSpan + leafSpeed/5
+          this.isFalling = true
+        } else if (this.lifeSpan > 640) {
+          // console.log('leaf is dead')
           this.isDead = true
         }
-        if (this.isDead === true) {
-          this.color = p.color(157,219,255)
-          this.color2 = p.color(157,219,255)
-          this.lifeSpan = this.lifeSpan - 3
-        } else if (this.isDead === false) {
-          this.lifeSpan = this.lifeSpan + 0.03
-        }
+        this.color1  = p.color(this.colorArray[0], this.colorArray[1], this.colorArray[2])
       }
 
-      clicked() {
-        const mouseVector = p.createVector(p.mouseX, p.mouseY)
-        const dist = this.location.dist(mouseVector)
-        if (dist < this.lifeSpan/2) {
-          this.isDead = true
-        }
-      }
-
-      // boyancy() {
-      //   let boyance
-      //   let lift
-      //   if (this.location.y > canvasHeight/5) {
-      //     lift = p.map(this.location.y, canvasHeight/5, canvasHeight, 0, -0.008)
-      //     boyance = p.createVector(0, lift)
-      //   } else {
-      //     boyance = p.createVector(0,0)
+      // checkDeath() {
+      //   if (this.lifeSpan > 640) {
+      //     this.isDead = true
       //   }
-      //   // console.log(boyance)
-      //   this.applyForce(boyance)
       // }
 
-      checkCollision(other) {
-        const otherLocation = other.location
-        // const mouseVector = p.createVector(p.mosueX, p.mouseY)
-        const dist = this.location.dist(otherLocation)
-        if (dist < ((other.lifeSpan/2)+(this.lifeSpan/2))) {
-          console.log('collision!')
-          this.color = p.color(255)
-          this.velocity.x = this.velocity.x * -1
-          this.velocity.y = this.velocity.y * -1
-          // this.isDead = true
-        }
-      }
+      // clicked() {
+      //   const mouseVector = p.createVector(p.mouseX, p.mouseY)
+      //   const dist = this.location.dist(mouseVector)
+      //   if (dist < this.lifeSpan/2) {
+      //     this.isDead = true
+      //   }
+      // }
 
-      separate(bubbles) {
-        // console.log('separating!')
-        const maxSpeed = 300
-        const maxForce = 300
-        let sum = new p5.Vector()
-        let count = 0
-        const desiredSeparation = 20
-        bubbles.forEach((bubble) => {
-          const distance = this.location.dist(bubble.location)
-          if ((distance > 0) && (distance < desiredSeparation)) {
-            console.log('spearating!!!!')
-            let diff = p.createVector()
-            diff = this.location.sub(bubble.location)
-            diff.normalize()
-            diff.div(distance)
-            sum.add(diff)
-            count ++
-          }
-        })
-        if (count > 0) {
-          sum.div(count)
-          sum.setMag(maxSpeed)
-          const steer = sum.sub(this.velocity)
-          steer.limit(maxForce)
-          this.applyForce(steer)
-        }
-      }
+      // checkCollision(other) {
+      //   const otherLocation = other.location
+      //   // const mouseVector = p.createVector(p.mosueX, p.mouseY)
+      //   const dist = this.location.dist(otherLocation)
+      //   if (dist < ((other.lifeSpan/2)+(this.lifeSpan/2))) {
+      //     console.log('collision!')
+      //     this.color = p.color(255)
+      //     this.velocity.x = this.velocity.x * -1
+      //     this.velocity.y = this.velocity.y * -1
+      //     // this.isDead = true
+      //   }
+      // }
+
+      // separate(bubbles) {
+      //   // console.log('separating!')
+      //   const maxSpeed = 300
+      //   const maxForce = 300
+      //   let sum = new p5.Vector()
+      //   let count = 0
+      //   const desiredSeparation = 20
+      //   bubbles.forEach((bubble) => {
+      //     const distance = this.location.dist(bubble.location)
+      //     if ((distance > 0) && (distance < desiredSeparation)) {
+      //       console.log('spearating!!!!')
+      //       let diff = p.createVector()
+      //       diff = this.location.sub(bubble.location)
+      //       diff.normalize()
+      //       diff.div(distance)
+      //       sum.add(diff)
+      //       count ++
+      //     }
+      //   })
+      //   if (count > 0) {
+      //     sum.div(count)
+      //     sum.setMag(maxSpeed)
+      //     const steer = sum.sub(this.velocity)
+      //     steer.limit(maxForce)
+      //     this.applyForce(steer)
+      //   }
+      // }
     }
 
     // ------------------------------------------------------------------------------------------ //
